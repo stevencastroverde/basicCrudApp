cd5807190d511daba5a4388cebd356fbe128944a
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2016-12-08 16:30:13 -0700

    fixed connection issues

--- knexfile.js
+++ knexfile.js
@@ -2,18 +2,21 @@
  const dotenv =require('dotenv')
  dotenv.config();
 
+
+module.exports = {
   development: {
     client: 'postgresql',
 
     connection: process.env.DEVELOPMENT_DATABASE_URL
 
-  };
+  },
 
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
+
 
 
 };
